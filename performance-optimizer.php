cd03fce842a82dc779c2c9efab4a65fad5a43cba
vikum <vikumrp@gmail.com>
2023-08-24 18:09:03 +0530

    Admin Page Callback Function

--- performance-optimizer.php
+++ performance-optimizer.php
@@ -24,3 +24,23 @@ function performance_optimizer_menu_page() {
     );
 }
 add_action('admin_menu', 'performance_optimizer_menu_page');
+
+// step 3 Admin Page Callback Function
+
+function performance_optimizer_admin_page() {
+    if (!current_user_can('manage_options')) {
+        return;
+    }
+
+    // Handle form submission and settings update here
+
+    ?>
+    <div class="wrap">
+        <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
+        <form method="post" action="">
+            <!-- Add your optimization settings fields here -->
+            <?php submit_button('Save Settings'); ?>
+        </form>
+    </div>
+    <?php
+}
