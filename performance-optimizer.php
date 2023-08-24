00f472c9bab40ab06bca6dcca05e5e38b967355d
vikum <vikumrp@gmail.com>
2023-08-24 18:13:57 +0530

    full code

--- performance-optimizer.php
+++ performance-optimizer.php
@@ -3,17 +3,16 @@
 Plugin Name: Performance Optimizer
 Description: Custom performance optimization features.
 Version: 1.0
-Author: vikum rajeewa 
+Author: Your Name
 */
 
-//step 1 
+// Enqueue admin CSS
 function performance_optimizer_enqueue_admin_scripts() {
     wp_enqueue_style('performance-optimizer-admin', plugins_url('admin.css', __FILE__));
 }
 add_action('admin_enqueue_scripts', 'performance_optimizer_enqueue_admin_scripts');
 
-//step 2 
-
+// Add admin menu page
 function performance_optimizer_menu_page() {
     add_menu_page(
         'Performance Optimizer',
@@ -25,20 +24,27 @@ function performance_optimizer_menu_page() {
 }
 add_action('admin_menu', 'performance_optimizer_menu_page');
 
-// step 3 Admin Page Callback Function
-
+// Admin page callback function
 function performance_optimizer_admin_page() {
     if (!current_user_can('manage_options')) {
         return;
     }
 
     // Handle form submission and settings update here
+    if (isset($_POST['performance_optimizer_settings'])) {
+        update_option('enable_caching', isset($_POST['enable_caching']) ? 1 : 0);
+    }
 
     ?>
     <div class="wrap">
         <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
         <form method="post" action="">
-            <!-- Add your optimization settings fields here -->
+            <label>
+                <input type="checkbox" name="enable_caching" value="1" <?php checked(get_option('enable_caching'), 1); ?>>
+                Enable Caching
+            </label>
+            <br>
+            <input type="hidden" name="performance_optimizer_settings" value="1">
             <?php submit_button('Save Settings'); ?>
         </form>
     </div>
